3e5eb230e307ba6535224118be9a074d0429ef6d
Johan Strand <53336235+johan-st@users.noreply.github.com>
2023-02-15 10:47:50 +0100

    remove Test.only

--- src/LivsmedelTests.elm
+++ src/LivsmedelTests.elm
@@ -8,32 +8,31 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    only <|
-        describe "FoodData" <|
-            [ test "test test test" <|
-                \() ->
-                    let
-                        expected =
-                            [ { namn = "Köttfärs"
-                              , id = 2
-                              , energi = 250
-                              , kolhydrater = 0
-                              , protein = 20
-                              , fett = 20
-                              }
-                            , { namn = "Köttbullar"
-                              , id = 3
-                              , energi = 250
-                              , kolhydrater = 10
-                              , protein = 20
-                              , fett = 20
-                              }
-                            ]
-                    in
-                    Data.filter "kött" smallSet
-                        |> Expect.equalLists
-                            expected
-            ]
+    describe "FoodData" <|
+        [ test "test test test" <|
+            \() ->
+                let
+                    expected =
+                        [ { namn = "Köttfärs"
+                          , id = 2
+                          , energi = 250
+                          , kolhydrater = 0
+                          , protein = 20
+                          , fett = 20
+                          }
+                        , { namn = "Köttbullar"
+                          , id = 3
+                          , energi = 250
+                          , kolhydrater = 10
+                          , protein = 20
+                          , fett = 20
+                          }
+                        ]
+                in
+                Data.filter "kött" smallSet
+                    |> Expect.equalLists
+                        expected
+        ]
 
 
 smallSet : List Data.Livsmedel
