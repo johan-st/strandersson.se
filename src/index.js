ce9ea46bdbfd593c7dfeb38b9e1b355c94162357
johan-st <johan@styldesign.se>
2023-03-12 19:19:00 +0100

    add feature flag

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { Elm } from "./Main.elm";
+import { getFeatureFlags, timedPromise, log } from "./helpers.js";
+
 // build time from environment variable
 const buildTag = process.env.BUILD_TAG;
 const nodeEnv = process.env.NODE_ENV;
+let debug = false;
 
-// Service worker
+// register service worker
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker
@@ -13,43 +16,53 @@ if ("serviceWorker" in navigator) {
     });
 }
 
-// decide what tag to show in the footer
-let build = "";
-if (nodeEnv == "production") {
-    build = buildTag
-} else {
-    build = " <DEV>:" + buildTag
-}
-// Get the data from localStorage, if it exists.
-// put the data in the flags for the elm app.
-const storedData = localStorage.getItem('FoodCalculator');
-const flags = {
-    foodCalculator: storedData ? JSON.parse(storedData) : null,
-    build
-}
-window.app = Elm.Main.init({
-    node: document.querySelector("body"),
-    flags: flags,
-});
 
-// Listen for commands from the `localStorageSet` port.
-// Turn the data to a string and put it in localStorage.
-window.app.ports.localStorageSet.subscribe(function (fc) {
-    localStorage.setItem('FoodCalculator', JSON.stringify(fc, null, 0));
-});
 
-// DEBUG: test the service worker
-// // setting up a timer to check for new versions of database
-// setInterval(() => {
-//     fetch('/LivsmedelsDB.json')
-//         .then(response => response.json())
-//         .then(data => {
-//             // check if the version is newer than the current one
-//             // if (data.version) {
-//             console.log("v" + data.version);
-//             // if so, send a message to elm
-//             // window.app.ports.newVersion.send(data.version);
-//             // }
-//         })
-// }, 5000)
-// // }, 1000 * 60 * 60)
+// INIYALIZE THE APP
+const ff = getFeatureFlags();
+const timeout = timedPromise(500);
+// wait for the feature flags to be loaded or timeout
+Promise.race([ff, timeout])
+    .then(ff => {
+        // Set the feature flags on window so they can be accessed from the service worker (what is a better way?)
+        if (ff.flags.includes("debug")) {
+            log("Debug mode enabled");
+            log(`featureFlags (version: ${ff.version}): `, ff.flags);
+            log("Build tag:", buildTag);
+            log("Node env:", nodeEnv);
+        }
+
+        // decide what tag to show in the footer
+        let build = "";
+        if (nodeEnv == "production") {
+            build = buildTag
+        } else {
+            build = " <DEV>:" + buildTag
+        }
+
+        // Get the data from localStorage, if it exists.
+        // put the data in the flags for the elm app.
+        const storedData = localStorage.getItem('FoodCalculator');
+        const flags = {
+            foodCalculator: storedData ? JSON.parse(storedData) : null,
+            build,
+            featureFlags: ff,
+        }
+        window.app = Elm.Main.init({
+            node: document.querySelector("body"),
+            flags: flags,
+        });
+
+        // Listen for commands from the `localStorageSet` port.
+        // Turn the data to a string and put it in localStorage.
+        window.app.ports.localStorageSet.subscribe(function (fc) {
+            localStorage.setItem('FoodCalculator', JSON.stringify(fc, null, 0));
+        });
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+log(true, "App started");
+
--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,8 @@
-const cacheName = "v20230312"; // Change value to force update
 
+const cacheName = "v20230312-3"; // Change value to force update
+
+
+// Register the service worker
 self.addEventListener("install", event => {
 	// Kick out the old service worker
 	self.skipWaiting();
@@ -15,7 +18,9 @@ self.addEventListener("install", event => {
 				"maskable_icon.png", // Favicon, maskable https://web.dev/maskable-icon
 				"style.css", // Main CSS file
 				"LivsmedelsDB.json", // Database of foodstuffs from Livsmedelsverket (Swedish Food Agency) 
-			]);
+			]).catch(error => {
+				console.log("Error caching assets", error);
+			});
 		})
 	);
 });
@@ -35,19 +40,29 @@ self.addEventListener("activate", event => {
 	);
 });
 
-// Offline-first, cache-first strategy
+// Offline-first, cache-first strategy (except for featureFlags.json)
 // Kick off two asynchronous requests, one to the cache and one to the network
 // If there's a cached version available, use it, but fetch an update for next time.
 // Gets data on screen as quickly as possible, then updates once the network has returned the latest data. 
 self.addEventListener("fetch", event => {
-	event.respondWith(
-		caches.open(cacheName).then(cache => {
-			return cache.match(event.request).then(response => {
-				return response || fetch(event.request).then(networkResponse => {
-					cache.put(event.request, networkResponse.clone());
-					return networkResponse;
-				});
-			})
+	// do not cache the featureFlags.json file
+	// TODO: make this support api calls with no extension
+	if (event.request.url.includes("featureFlags.json")) {
+		console.log("featureFlags.json", event.request.url)
+		fetch(event.request).then(networkResponse => {
+			return networkResponse;
 		})
-	);
-});
+	} else {
+		event.respondWith(
+			caches.open(cacheName).then(cache => {
+				return cache.match(event.request).then(response => {
+					return response || fetch(event.request).then(networkResponse => {
+						cache.put(event.request, networkResponse.clone());
+						return networkResponse;
+					});
+				})
+			}))
+	}
+})
+
+// TODO: unregister service worker if feature flag is removed
